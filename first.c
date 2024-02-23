fff827c9a7bfe52e8f8b748c268c8511a8eddd71
Rohit <rohitkusv@gmail.com>
2024-02-23 12:09:51 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,10 @@
+//Take a number(n) from user & output its cube(n*n*n).
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the no n");
+    scanf("%d",&n);
+     
+     n =n*n*n;
+     printf("the answer is %d",n);
+}
